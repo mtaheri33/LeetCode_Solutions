7ef329a700a44360f9084869039485a7f9131226
mtaheri33 <michael.taheri33@gmail.com>
2023-01-19 21:30:49 -0800

    solved 121 Best Time to Buy and Sell Stock

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -1,6 +1,6 @@
-# 1. Two Sum
 def twoSum(nums, target):
     """
+    1. Two Sum
     This takes in a list of integers, nums, and a single integer,
     target.  It returns a list of two integers that are the indices of
     values in nums which sum to target.  The two indices in the list
@@ -8,6 +8,7 @@ def twoSum(nums, target):
     """
     # The format of the dictionary is number: index.
     mydict = {}
+
     # This iterates through nums.  For each value, it calculates the
     # difference that is target - value.  It checks if that difference
     # was a previous value in nums.  If it was, then the solution is
@@ -20,9 +21,46 @@ def twoSum(nums, target):
         if difference in mydict:
             # The difference was a previous value in nums.  This
             # returns the current index and the index of the previous
-            # value. 
+            # value.
             return [i, mydict[difference]]
         else:
             # The difference was not a previous value in nums.  This
             # adds the number and its index to the dictionary.
             mydict[current_number] = i
+
+
+def maxProfit(prices):
+    """
+    121. Best Time to Buy and Sell Stock
+    This takes in a list of prices that represent stock prices each
+    day.  It returns an integer of the maximum profit that can be
+    achieved if someone buys a stock one day and sells it on a
+    different day in the future.  If no profit is possible, it returns
+    0.
+    """
+    # The lowest price
+    lowest_price = prices[0]
+    max_profit = 0
+
+    # This iterates through each price, and as it does so it keeps
+    # track of the maximum profit as well as lowest price up to that
+    # point.
+    # For the price each day, it subtracts the past lowest price in
+    # order to calculate the profit of selling on that day.  If this
+    # value is greater than a previously calculated profit amount, a
+    # new maximum has been found.  In addition, it checks if the price
+    # that day is a new lowest price.
+    for price in prices:
+        profit = price - lowest_price
+        if profit > max_profit:
+            # Selling on this day is a new maximum profit.
+            max_profit = profit
+        if price < lowest_price:
+            # The price on this day is a new lowest price.
+            lowest_price = price
+
+    # If no profit was possible because the profit each day was 0 or
+    # lower, the variable max_profit was never changed from its initial
+    # value of 0.  In this case, 0 is returned.  Otherwise, the maximum
+    # profit is returned.
+    return max_profit
